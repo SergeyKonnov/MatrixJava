1db917fae8cd2ff8ba5c9429a41cb75a0ed3fb55
Sergey Konnov <sergeyzorro007@gmail.com>
2020-04-28 21:57:40 +0300

    matrix class

---
+++ src/main/java/Matrix.java
@@ -0,0 +1,75 @@
+public class Matrix {
+
+    public static double[][] multiply(double[][] firstMatrix, double[][] secondMatrix) {
+        double[][] result = new double[firstMatrix.length][secondMatrix[0].length];
+
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = multiplyMatricesCell(firstMatrix, secondMatrix, row, col);
+            }
+        }
+        return result;
+    }
+
+    public static double[][] add(double[][] firstMatrix, double[][] secondMatrix) throws Exception {
+        if(firstMatrix.length != secondMatrix.length || firstMatrix[0].length != secondMatrix[0].length)
+            throw new Exception("non-equal sizes");
+        double[][] result = new double[firstMatrix.length][firstMatrix[0].length];
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = firstMatrix[row][col] + secondMatrix[row][col];
+            }
+        }
+        return result;
+    }
+
+    public static double[][] subtract(double[][] firstMatrix, double[][] secondMatrix) throws Exception {
+        if(firstMatrix.length != secondMatrix.length || firstMatrix[0].length != secondMatrix[0].length)
+            throw new Exception("non-equal sizes");
+        double[][] result = new double[firstMatrix.length][firstMatrix[0].length];
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = firstMatrix[row][col] - secondMatrix[row][col];
+            }
+        }
+        return result;
+    }
+
+    public static double[][] add(double[][] firstMatrix, double number) {
+        double[][] result = new double[firstMatrix.length][firstMatrix[0].length];
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = firstMatrix[row][col] + number;
+            }
+        }
+        return result;
+    }
+
+    public static double[][] subtract(double[][] firstMatrix, double number) {
+        double[][] result = new double[firstMatrix.length][firstMatrix[0].length];
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = firstMatrix[row][col] - number;
+            }
+        }
+        return result;
+    }
+
+    private static double multiplyMatricesCell(double[][] firstMatrix, double[][] secondMatrix, int row, int col) {
+        double cell = 0;
+        for (int i = 0; i < secondMatrix.length; i++) {
+            cell += firstMatrix[row][i] * secondMatrix[i][col];
+        }
+        return cell;
+    }
+
+    public static void print(double[][] matrix) {
+        int rows=matrix.length;
+        int cols=matrix[0].length;
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++)
+                System.out.print(matrix[i][j]+" ");
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/program.java
@@ -0,0 +1,7 @@
+public class program {
+    public static void main(String[] args) {
+        Matrix matrix = new Matrix();
+        double[][] matr = {{1, 2 ,3}, {1, 2 ,3}, {1, 2 ,3}};
+        matrix.print(matr);
+    }
+}
